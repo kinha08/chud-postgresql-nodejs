b07239c4647476296ca4ac102c293e7c674ebb3a
Lucas Nobre <lucasnobredasilva@gmail.com>
2022-07-04 15:47:08 -0300

    add restrict

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -1,10 +1,26 @@
 import {query} from '../config/database.js';
 
+// Create a new customer
 const createCustomer = async (req, res) => {
     const {name, phone1, phone2} = req.body;
-    const {rows} = await query(
+
+    // Check if customer already exist and return a messagem
+    const user = await query(
+        "SELECT * FROM fonelight.customers WHERE nome = $1",
+        [name],
+    );
+
+    if (user.rowCount > 0) {
+        res.status(409).send({
+            message: "User already exist",
+        })
+        return;
+    }
+
+    // Create customer
+    const response = await query(
         "INSERT INTO fonelight.customers (nome, phone1, phone2) VALUES($1, $2, $3)",
-        [name, phone1, phone2],
+        [name.toUpperCase(), phone1, phone2],
     );
 
     res.status(200).json({
@@ -24,15 +40,21 @@ const listAllCustomers = async (req, res) => {
         'SELECT * FROM fonelight.customers ORDER BY nome ASC'
     );
     res.status(200).send(response.rows);
-}
+};
 
 const findCustomerById = async (req, res) => {
     const customerId = parseInt(req.params.id);
     const response = await query(
         'SELECT * FROM fonelight.customers WHERE id = $1', [customerId],
     );
+
+    if (response.rowCount === 0) {
+        return res.status(400).send({
+            message: "User not found",
+        }).end();
+    }
     res.status(200).send(response.rows);
-}
+};
 
 const updateCustomerById = async (req, res) => {
     const customerId = parseInt(req.params.id);
@@ -46,7 +68,7 @@ const updateCustomerById = async (req, res) => {
     res.status(200).send({
         message: 'Customer updated successfully!',
     });
-}
+};
 
 const deleteCustomerById = async (req, res) => {
     const customerId = parseInt(req.params.id);
@@ -58,7 +80,7 @@ const deleteCustomerById = async (req, res) => {
         message: 'Customer deleted successfully!',
         customerId,
     })
-}
+};
 
 
 export {
